f8473b8c26566bf87e2a21739ed9f47bc1ddf782
Alexander Thurn <at@froso.de>
2017-05-16 16:01:36 +0200

    getOptions instead of parseQuery

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-(function(){"use strict";var a,b,c,d;a=require("lodash"),c=require("path"),d=require("preprocess"),b=require("loader-utils"),module.exports=function(e){var f,g,h,i,j,k;return"function"==typeof this.cacheable&&this.cacheable(),k=c.extname(this.resourcePath||"").split("."),g=k[k.length-1],h={srcDir:this.context,type:g},j=b.parseQuery(this.query),a.isObject(j.ppOptions)&&(h=a.merge(h,j.ppOptions),delete j.ppOptions),/cjsx/i.test(h.type)&&(h.type="coffee"),f=a.merge({},process.env,j),f.NODE_ENV=f.NODE_ENV||"development",i=d.preprocess(e,f,h),this.callback(null,i)}}).call(this);
+(function () {
+    "use strict";
+    var a, b, c, d;
+    a = require("lodash"), c = require("path"), d = require("preprocess"), b = require("loader-utils"), module.exports = function (e) {
+        var f, g, h, i, j, k;
+        return "function" == typeof this.cacheable && this.cacheable(), k = c.extname(this.resourcePath || "").split("."), g = k[k.length - 1], h = {
+            srcDir: this.context,
+            type: g
+        }, j = b.getOptions(this), a.isObject(j.ppOptions) && (h = a.merge(h, j.ppOptions), delete j.ppOptions), /cjsx/i.test(h.type) && (h.type = "coffee"), f = a.merge({}, process.env, j), f.NODE_ENV = f.NODE_ENV || "development", i = d.preprocess(e, f, h), this.callback(null, i)
+    }
+}).call(this);
