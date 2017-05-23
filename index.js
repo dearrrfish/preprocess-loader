a9aa435acd9ed9bd34e9c5a22a70f106dc88800f
Yu Jin <dearrrfish@gmail.com>
2017-05-23 13:14:41 -0700

    Use `getOptions` instead of deprecated `parseQuery` to get options.

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-(function () {
-    "use strict";
-    var a, b, c, d;
-    a = require("lodash"), c = require("path"), d = require("preprocess"), b = require("loader-utils"), module.exports = function (e) {
-        var f, g, h, i, j, k;
-        return "function" == typeof this.cacheable && this.cacheable(), k = c.extname(this.resourcePath || "").split("."), g = k[k.length - 1], h = {
-            srcDir: this.context,
-            type: g
-        }, j = b.getOptions(this), a.isObject(j.ppOptions) && (h = a.merge(h, j.ppOptions), delete j.ppOptions), /cjsx/i.test(h.type) && (h.type = "coffee"), f = a.merge({}, process.env, j), f.NODE_ENV = f.NODE_ENV || "development", i = d.preprocess(e, f, h), this.callback(null, i)
-    }
-}).call(this);
+(function(){"use strict";var e,t,s,r;e=require("lodash"),s=require("path"),r=require("preprocess"),t=require("loader-utils"),module.exports=function(i){var p,c,o,n,l,a;return"function"==typeof this.cacheable&&this.cacheable(),a=s.extname(this.resourcePath||"").split("."),c=a[a.length-1],o={srcDir:this.context,type:c},l=t.getOptions(this),e.isObject(l.ppOptions)&&(o=e.merge(o,l.ppOptions),delete l.ppOptions),/cjsx/i.test(o.type)&&(o.type="coffee"),p=e.merge({},process.env,l),p.NODE_ENV=p.NODE_ENV||"development",n=r.preprocess(i,p,o),this.callback(null,n)}}).call(this);
